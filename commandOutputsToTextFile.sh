1046d1ac7f17b69cd2d13f7e20d25b4a83bc2599
JoeJunkerATU <G00438811@atu.ie>
2024-04-17 17:42:09 +0000

    Appended command executions to text file and used echo to format headings and spaces.Also made sure file is cleared before loop starts

--- commandOutputsToTextFile.sh
+++ commandOutputsToTextFile.sh
@@ -19,12 +19,16 @@ commands_array=(
     "top -n 1 -b"
     "history 50"
 )
+#clear file before writing to it
 
-
+> "JoeJunker.txt"
 #create for loop to iterate over the array and execute commands.
 
 for commands in "${commands_array[@]}"; do
-
-	eval "$commands"
-
+        #add echo commands for formatting 
+      echo -e "\n" >> "JoeJunker.txt"
+      echo "------------------------------- $commands command ---------------------------------" >> "JoeJunker.txt"
+      echo -e "\n" >> "JoeJunker.txt"
+      eval "$commands" >> "JoeJunker.txt" 
+      echo -e "\n" >> "JoeJunker.txt"
 done
