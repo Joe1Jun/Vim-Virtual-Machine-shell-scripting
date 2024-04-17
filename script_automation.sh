a749d5f21b3ebedc748082038346d4648c444e2f
JoeJunkerATU <G00438811@atu.ie>
2024-04-17 14:51:08 +0000

    Redirected bash_history file inside the .sh file to enable history command

--- script_automation.sh
+++ script_automation.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#Redirects bash_historyfile inside the .sh file
+#Include at top of script to enable the history command
+
+HISTFILE=~/.bash_history
+set -o history
 echo Hello World
 date
 hostname
@@ -9,5 +14,10 @@ uptime
 whoami
 who
 w
+
+#-b puts top in batch mode which produces output suitable for saving to a file rather than displaying interactively
+#-n1 limit the iterations of top to the specified number
 top -b -n1
+
 history
+ 
