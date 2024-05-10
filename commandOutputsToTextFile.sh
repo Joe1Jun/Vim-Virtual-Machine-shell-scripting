d431546f1c242e8d4e15b19ed06899b9de507abe
JoeJunkerATU <G00438811@atu.ie>
2024-05-10 17:05:41 +0000

    Removed last command and reverted back to output redirection inline with the script rather than an array loop as per instruction

--- commandOutputsToTextFile.sh
+++ commandOutputsToTextFile.sh
@@ -1,34 +1,58 @@
-#!/bin/bash 
+#!/bin/bash
+
+
+#Redirects bash_historyfile inside the .sh file
+#Include at top of script to enable the history command
 
 HISTFILE=~/.bash_history
 set -o history
 
-#create an array of commands.
-
-commands_array=(
-    "echo Hello World"
-    "date"
-    "hostname"
-    "arch"
-    "uname -a"
-    "uptime"
-    "whoami"
-    "who"
-    "last"
-    "w"
-    "top -n 1 -b"
-    "history 50"
-)
-#clear file before writing to it
-
-> "JoeJunker.txt"
-#create for loop to iterate over the array and execute commands.
-
-for commands in "${commands_array[@]}"; do
-        #add echo commands for formatting 
-      echo -e "\n" >> "JoeJunker.txt"
-      echo "------------------------------- $commands command ---------------------------------" >> "JoeJunker.txt"
-      echo -e "\n" >> "JoeJunker.txt"
-      eval "$commands" >> "JoeJunker.txt" 
-      echo -e "\n" >> "JoeJunker.txt"
-done
+# > writes to file >> appends to existing file
+# create spaces and headings with echo.
+# > at the beginning overwrites the file each time the shellscript is executed.
+# >> appends everything afterwards to the new file content.
+echo "------------- echo command -----------------" > JoeJunker.txt
+# -e used so the string \n is interpreted as a new line command not a printable string.
+echo -e "\n" >> JoeJunker.txt
+echo Hello World >> JoeJunker.txt
+echo -e "\n" >> JoeJunker.txt
+echo "------------- date command -----------------" >> JoeJunker.txt
+echo -e "\n" >> JoeJunker.txt
+date >> JoeJunker.txt
+echo -e "\n" >> JoeJunker.txt
+echo "------------- hostname command -----------------" >> JoeJunker.txt
+echo -e "\n" >> JoeJunker.txt
+hostname >> JoeJunker.txt
+echo -e "\n" >> JoeJunker.txt
+echo "------------- arch command -----------------" >> JoeJunker.txt
+echo -e "\n" >> JoeJunker.txt
+arch >> JoeJunker.txt
+echo -e "\n" >> JoeJunker.txt
+echo "------------- uname command -----------------" >> JoeJunker.txt
+echo -e "\n" >> JoeJunker.txt
+uname -a >> JoeJunker.txt
+echo -e "\n" >> JoeJunker.txt
+echo "------------- uptime command -----------------" >> JoeJunker.txt
+echo -e "\n" >> JoeJunker.txt
+uptime >> JoeJunker.txt:
+echo -e "\n" >> JoeJunker.txt
+echo "------------- whomai command -----------------" >> JoeJunker.txt
+echo -e "\n" >> JoeJunker.txt
+whoami >> JoeJunker.txt
+echo -e "\n" >> JoeJunker.txt
+echo "------------- who command -----------------" >> JoeJunker.txt
+echo -e "\n" >> JoeJunker.txt
+who >> JoeJunker.txt
+echo -e "\n" >> JoeJunker.txt
+echo "------------- w command -----------------" >> JoeJunker.txt
+echo -e "\n" >> JoeJunker.txt
+w >> JoeJunker.txt
+echo -e "\n" >> JoeJunker.txt
+echo "------------- top command -----------------" >> JoeJunker.txt
+echo -e "\n" >> JoeJunker.txt
+top -b -n1 >> JoeJunker.txt
+history >> JoeJunker.txt
+echo -e "\n" >> JoeJunker.txt
+echo "------------- history command -----------------" >> JoeJunker.txt
+echo -e "\n" >> JoeJunker.txt
+history 50  >> JoeJunker.txt
